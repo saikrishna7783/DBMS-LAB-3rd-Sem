b04efe514a6d8979dc61e9804ee1f977fc966c3f
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2022-12-19 22:59:25 +0530

    Create Supplier Database.sql

---
+++ Supplier Database.sql
@@ -0,0 +1,53 @@
+show databases;
+create database if not exists 1bm21cs123_supplier;
+use 1bm21cs123_supplier;
+show tables;
+
+create table supplier(
+sid int,
+sname varchar(20),
+city varchar(20),
+primary key (sid)
+);
+select * from supplier;
+insert into supplier
+values(10001,'Acme Widget','Bangalore'),
+(10002,'Johns','Kolkata'),
+(10003,'Vimal','Mumbai'),
+(10004,'Reliance','Delhi');
+
+create table parts(
+pid int,
+pname varchar(20),
+color varchar(20),
+primary key (pid)
+);
+select * from parts;
+insert into parts
+values(20001,'Book','Red'),
+(20002,'Pen','Red'),
+(20003,'Pencil','Green'),
+(20004,'Mobile','Green'),
+(20005,'Charger','Black');
+
+create table catalog(
+sid int,
+pid int,
+cost int,
+primary key (sid,pid),
+foreign key (sid) references supplier(sid)
+on delete cascade on update cascade,
+foreign key (pid) references parts(pid)
+on delete cascade on update cascade
+);
+select * from catalog;
+insert into catalog
+values(10001,20001,10),
+(10001,20002,10),
+(10001,20003,30),
+(10001,20004,10),
+(10001,20005,10),
+(10002,20001,10),
+(10002,20002,20),
+(10003,20003,30),
+(10004,20003,40);
